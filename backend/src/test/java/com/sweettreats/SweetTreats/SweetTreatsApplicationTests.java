2bf16b418ee86ba808b856374e62031b1aaa7982
sofiatorres25 <sofiatorres5082@gmail.com>
2025-04-12 21:14:43 -0300

    init: backend

---
+++ backend/src/test/java/com/sweettreats/SweetTreats/SweetTreatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweettreats.SweetTreats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetTreatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
